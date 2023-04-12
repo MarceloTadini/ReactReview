f69273e7279c752557f813ff7b644c993941aae7
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-04-12 19:43:58 -0300

    React props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header";
+
 
 function App(){
     return(
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <>
+            <Header titulo="Hello World!"></Header>
+        </>
     )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header({titulo}) {
+    return(
+        <header>
+            {titulo}
+        </header>
+    )
+}
+
+export default Header
